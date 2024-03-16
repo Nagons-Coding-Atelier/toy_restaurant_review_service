02a8421252ee863dc79181cd20d4818a6b2c9297
Nagons-Coding-Atelier <nagon.atelier@gmail.com>
2024-03-16 22:19:48 +0900

    init backend

---
+++ toy_restaurant_review_service_backend/src/main/java/me/nagons/toy_restaurant_review_service_backend/ToyRestaurantReviewServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package me.nagons.toy_restaurant_review_service_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyRestaurantReviewServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyRestaurantReviewServiceBackendApplication.class, args);
+	}
+
+}
---
+++ toy_restaurant_review_service_backend/src/test/java/me/nagons/toy_restaurant_review_service_backend/ToyRestaurantReviewServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nagons.toy_restaurant_review_service_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyRestaurantReviewServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
